99f25ae482dcb68a5d2c7b843cbbf99f24606667
= <vs4622@srmist.edu.in>
2026-03-11 16:01:31 +0530

    Added UC6

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,44 @@
+/**
+ * MAIN CLASS - BookMyStayApp
+ *
+ * Use Case 6: Reservation Confirmation & Room Allocation
+ *
+ * Description:
+ * This class demonstrates how booking
+ * requests are confirmed and rooms
+ * are allocated safely.
+ *
+ * It consumes booking requests in FIFO
+ * order and updates inventory immediately.
+ *
+ * @version 6.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Room Allocation Processing\n");
+
+        // Initialize inventory
+        RoomInventory inventory = new RoomInventory();
+
+        // Initialize booking queue
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        // Create booking requests
+        bookingQueue.addRequest(new Reservation("Abhi", "Single Room"));
+        bookingQueue.addRequest(new Reservation("Subha", "Single Room"));
+        bookingQueue.addRequest(new Reservation("Vanmathi", "Suite Room"));
+
+        // Allocation service
+        RoomAllocationService allocationService = new RoomAllocationService();
+
+        // Process queue in FIFO order
+        while (bookingQueue.hasPendingRequests()) {
+
+            Reservation reservation = bookingQueue.getNextRequest();
+
+            allocationService.allocateRoom(reservation, inventory);
+        }
+    }
+}
