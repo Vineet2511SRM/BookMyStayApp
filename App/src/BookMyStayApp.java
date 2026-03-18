b7edf99834b15a4702eb010c8fdda2d8e0959340
VINEET2511SRM <emperorvineet7@gmail.com>
2026-03-18 14:15:27 +0530

    Vineet Added UC12-DataPersistence_SystemRecovery

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,49 +1,29 @@
 /**
  * MAIN CLASS - BookMyStayApp
  *
- * Use Case 11: Concurrent Booking Simulation
+ * Use Case 12: Data Persistence & System Recovery
  *
- * @version 11.0
+ * @version 12.0
  */
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Concurrent Booking Simulation\n");
+        System.out.println("System Recovery\n");
 
-        // Shared components
+        String filePath = "inventory.txt";
+
+        // Initialize inventory
         RoomInventory inventory = new RoomInventory();
-        BookingRequestQueue bookingQueue = new BookingRequestQueue();
-        RoomAllocationService allocationService = new RoomAllocationService();
 
-        // Add booking requests
-        bookingQueue.addRequest(new Reservation("Abhi", "Single Room"));
-        bookingQueue.addRequest(new Reservation("Subha", "Single Room"));
-        bookingQueue.addRequest(new Reservation("Vanmathi", "Double Room"));
-        bookingQueue.addRequest(new Reservation("Kural", "Suite Room"));
+        // Persistence service
+        FilePersistenceService persistenceService = new FilePersistenceService();
 
-        // Create threads
-        Thread t1 = new Thread(
-                new ConcurrentBookingProcessor(bookingQueue, inventory, allocationService)
-        );
+        // Load previous state
+        persistenceService.loadInventory(inventory, filePath);
 
-        Thread t2 = new Thread(
-                new ConcurrentBookingProcessor(bookingQueue, inventory, allocationService)
-        );
-
-        // Start threads
-        t1.start();
-        t2.start();
-
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            System.out.println("Thread execution interrupted.");
-        }
-
-        // Show remaining inventory
-        System.out.println("\nRemaining Inventory:");
+        // Display current inventory
+        System.out.println("\nCurrent Inventory:");
 
         System.out.println("Single Room: " +
                 inventory.getRoomAvailability().get("Single Room"));
@@ -53,5 +33,8 @@ public class BookMyStayApp {
 
         System.out.println("Suite Room: " +
                 inventory.getRoomAvailability().get("Suite Room"));
+
+        // Save state before exit
+        persistenceService.saveInventory(inventory, filePath);
     }
 }
